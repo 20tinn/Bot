cb77cb453e80bc3805c5378d37241593b6a0a2a6
20tinn <77859472+20tinn@users.noreply.github.com>
2021-02-16 13:57:23 -0300

    Update help.js

--- src/help.js
+++ src/help.js
@@ -1,125 +1,124 @@
 const help = (prefix) => {
 	return `
 <══════════════════════>
-      *BOT DO CAUSS*
-v:1.0  <══════════════════════>
+         *BOT DO 20TIN*
+<══════════════════════>
 
 
 
 😐 *info do bot*
-  🐉 Prefix: >  *  <
-  🐉 Dono: CAUSS  
-  🐉 Canal do criador: https://youtube.com/channel/UCpB3qh2Sp3K23s9a2Q-Gf-g
-  🐉 *Parcerias*: *parceria
+  ⚡ Prefix: >  *  <
+  ⚡ Dono: 20TIN (base: causs  
+  
 😐 *SOBRE*
   │
-  ├─ 🐉 ${prefix}info
-  ├─ 🐉 ${prefix}blocklist
-  ├─ 🐉 ${prefix}chatlist
-  ├─ 🐉 ${prefix}ping
-  ├─ 🐉 ${prefix}dono
-  ├─ 🐉 ${prefix}appbot
-  └─ 🐉 ${prefix}bugreport 
+  ├─ ⚡ ${prefix}info
+  ├─ ⚡ ${prefix}blocklist
+  ├─ ⚡ ${prefix}chatlist
+  ├─ ⚡ ${prefix}ping
+  ├─ ⚡ ${prefix}dono
+  ├─ ⚡ ${prefix}appbot
+  └─ ⚡ ${prefix}bugreport 
 😐 *favoritos*
   │
-  ├─ 🐉 ${prefix}sticker
-  ├─ 🐉 ${prefix}play 
-  ├─ 🐉 ${prefix}stickergif
-  ├─ 🐉 ${prefix}toimg
-  ├─ 🐉 *tomp3
-  ├─ 🐉 *marvellogo 
-  ├─ 🐉 *3dtext
-  ├─ 🐉 *phlogo
-  ├─ 🐉 *glitch
-  ├─ 🐉 *neonlogo
-  ├─ 🐉 *lionlogo
-  ├─ 🐉 *jokerlogo
-  ├─ 🐉 *coffee
-  ├─ 🐉 *lovepaper
+  ├─ ⚡ ${prefix}sticker
+  ├─ ⚡ ${prefix}play 
+  ├─ ⚡ ${prefix}stickergif
+  ├─ ⚡ ${prefix}toimg
+  ├─ ⚡ *tomp3
+  ├─ ⚡ *marvellogo 
+  ├─ ⚡ *3dtext
+  ├─ ⚡ *phlogo
+  ├─ ⚡ *glitch
+  ├─ ⚡ *neonlogo
+  ├─ ⚡ *lionlogo
+  ├─ ⚡ *jokerlogo
+  ├─ ⚡ *coffee
+  ├─ ⚡ *lovepaper
   
 😐 *MEDIA*
   │
-  ├─ 🐉 *trendtwit
-  ├─ 🐉 *randomkpop
-  └─ 🐉 *ytsearch
+  ├─ ⚡ *trendtwit
+  ├─ ⚡ *randomkpop
+  └─ ⚡ *ytsearch
 😐 *uiui, nada de colar*
   │
-  ├─ 🐉 *wiki
-  ├─ 🐉 *wikien
-  ├─ 🐉 *nulis
-  └─ 🐉 *Google
+  ├─ ⚡ *wiki
+  ├─ ⚡ *wikien
+  ├─ ⚡ *nulis
+  └─ ⚡ *Google
 
 😐 *DOWNLOADER*
   │
-  ├─ 🐉 *images
-  ├─ 🐉 *ytmp3
-  ├─ 🐉 *ytmp4
-  ├─ 🐉 *tiktok
-  └─ 🐉 *joox
+  ├─ ⚡ *images
+  ├─ ⚡ *ytmp3
+  ├─ ⚡ *ytmp4
+  ├─ ⚡ *tiktok
+  └─ ⚡ *joox
 😐 *MEME*
   │
-  ├─ 🐉 *meme
-  └─ 🐉 *memeindo
+  ├─ ⚡ *meme
+  └─ ⚡ *memeindo
 😐 *SOM*
   │
-  ├─ 🐉 *play
-  └─ 🐉 *tts
+  ├─ ⚡ *play
+  └─ ⚡ *tts pt
 😐 *MÚSICA*
   │
-  ├─ 🐉 *lirik
-  └─ 🐉 *chord
+  ├─ ⚡ *lirik
+  └─ ⚡ *chord
 😐 *ISLAM*
   │
-  └─ 🐉 *quran
+  └─ ⚡ *quran
 😐 *STALK*
   │
-  ├─ 🐊 *tiktokstalk
-  └─ 🐊 *igstalk
+  ├─ ⚡ *tiktokstalk
+  └─ ⚡ *igstalk
 😐 *WIBU*
   │
-  ├─ 🐉 *neonime
-  ├─ 🐉 *pokemon
-  ├─ 🐉 *loli
-  ├─ 🐉 *waifu
-  ├─ 🐉 *randomanime
-  ├─ 🐉 *husbu
-  ├─ 🐉 *husbu2
-  ├─ 🐉 *wait
-  └─ 🐉 *nekonime
+  ├─ ⚡ *neonime
+  ├─ ⚡ *pokemon
+  ├─ ⚡ *loli
+  ├─ ⚡ *waifu
+  ├─ ⚡ *randomanime
+  ├─ ⚡ *husbu
+  ├─ ⚡ *husbu2
+  ├─ ⚡ *wait
+  └─ ⚡ *nekonime
 😐 *DIVERSÃO*
   │
-  ├─ 🐉 *alay
-  ├─ 🐉 *gantengcek
-  ├─ 🐉 *watak
-  ├─ 🐉 *hobby
-  ├─ 🐉 *game
-  ├─ 🐉 *bucin
-  ├─ 🐉 *trust
-  ├─ 🐉 *dare
-  └─ 🐉 *simi
+  ├─ ⚡ *alay
+  ├─ ⚡ *gantengcek
+  ├─ ⚡ *watak
+  ├─ ⚡ *hobby
+  ├─ ⚡ *game
+  ├─ ⚡ *bucin
+  ├─ ⚡ *trust
+  ├─ ⚡ *dare
+  └─ ⚡ *simi
 😐 *INFORMAÇÃO*
   │
-  ├─ 🐉 *bahasa
-  └─ 🐉 *covid
-😐 *COMANDOS DO CAUSS*
+  ├─ ⚡ *bahasa
+  └─ ⚡ *covid
+😐 *COMANDOS DO 20TIN*
   │
-  ├─ 🐉 *setprefix
-  ├─ 🐉 *block
-  ├─ 🐉 *bc
-  ├─ 🐉 *bcgc
-  ├─ 🐉 *clone
-  └─ 🐉 *clearall
+  ├─ ⚡ *setprefix
+  ├─ ⚡ *block
+  ├─ ⚡ *bc
+  ├─ ⚡ *bcgc
+  ├─ ⚡ *clone
+  └─ ⚡ *clearall
 😐 *aleatório*
   │
-  ├─ 🐉 *send
-  ├─ 🐉 *wame
-  ├─ 🐉 *virtex
-  ├─ 🐉 *exe
-  ├─ 🐉 *qrcode
-  ├─ 🐉 *afk
-  ├─ 🐉 *timer
-  ├─ 🐉 *fml
-  └─ 🐉 *fml2
+  ├─ ⚡ *send
+  ├─ ⚡ *wame
+  ├─ ⚡ *virtex
+  ├─ ⚡ *exe
+  ├─ ⚡ *qrcode
+  ├─ ⚡ *afk
+  ├─ ⚡ *timer
+  ├─ ⚡ *fml
+  └─ ⚡ *fml2
 
    ( *CASO O BOT NÃO RESPONDA MANDE O COMANDO NOVAMENTE, NADA DE FLOOD* )
 `
